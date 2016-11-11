c28f96db2e1b2b8c9aeaf252822071893c779542
Aaron Black <aaron.black@complex.com>
2016-11-11 12:31:36 -0500

    break out AppHeader component

---
+++ src/AppHeader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from './logo.svg';
+
+const AppHeader = () => (
+  <div className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <h2>Welcome to React</h2>
+  </div>
+);
+
+export default AppHeader;
